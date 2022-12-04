047a615c86154fb59914296d62cf6c1d6dafa6ce
bakerbuilt1728 <52222329+bakerbuilt1728@users.noreply.github.com>
2022-12-04 13:22:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+
+    var rate = document.getElementById("rate").value;
+
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate /100;
+
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerHTML = 'If you deposit <span class="result_number">$'+parseInt(principal).toLocaleString()+'</span>, <br/>at an interest rate of <span class="result_number">'+rate+'%</span>. <br/>You will receive an amount of <span class="result_number">$'+parseFloat(interest).toLocaleString()+'</span>, <br/>in the year <span class="result_number">'+year+'</span>  <br/>'
     
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
